29557df8cebd62f256562fd07a0fb70850b934ac
Victor Kolebanov <kolebanov@gmail.com>
2022-04-18 19:43:36 +0300

    didnt work requests module

---
+++ parsing_training.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://health-diet.ru/table_calorie/'
+
+headers = {
+    'accept': '*/*',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
+}
+req = requests.get(url, headers=headers)
+src = req.text
+print(src)
